88cafb11e189ded1d583adfdb1f3642423f9ed68
Kenneth Gjaeringen <kgjaeringen@gmail.com>
2023-02-26 13:09:46 +0000

    Add files via upload

---
+++ web_scraper/scraper.py
@@ -0,0 +1,52 @@
+"""
+Simple web scraper extracting information from Steam.
+"""
+
+import requests
+import lxml.html
+import pandas as pd
+
+
+try:
+    html = requests.get('https://store.steampowered.com/explore/new/', timeout = (3, 5))
+    doc = lxml.html.fromstring(html.content)
+
+    print(html.status_code)
+
+    new_releases = doc.xpath('//div[@id="tab_newreleases_content"]')[0]
+    titles = new_releases.xpath('.//div[@class="tab_item_name"]/text()')
+    prices = new_releases.xpath('.//div[@class="discount_final_price"]/text()')
+
+    tags_divs = new_releases.xpath('.//div[@class="tab_item_top_tags"]')
+    tags = []
+    for div in tags_divs:
+        tags.append(div.text_content())
+
+    tags = [tag.split(',') for tag in tags]
+    platforms_div = new_releases.xpath('.//div[@class="tab_item_details"]')
+    total_platforms = []
+
+    for game in platforms_div:
+        temp = game.xpath('.//span[contains(@class, "platform_img")]')
+        platforms = [t.get('class').split(' ')[-1] for t in temp]
+        if 'hmd_separator' in platforms:
+            platforms.remove('hmd_separator')
+        total_platforms.append(platforms)
+
+    output = []
+    for info in zip(titles, prices, tags, total_platforms):
+        resp = {}
+        resp['title'] = info[0]
+        resp['price'] = info[1]
+        resp['tags'] = info[2]
+        resp['platforms'] = info[3]
+        output.append(resp)
+
+    df = pd.DataFrame(output)
+
+    print(df.head())
+
+except requests.exceptions.Timeout:
+    print("The request timed out")
+except requests.exceptions.RequestException as e:
+    print("An error occurred:", e)
